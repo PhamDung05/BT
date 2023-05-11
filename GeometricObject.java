3c355022f632601628d15a67544d3de99b76fc85
PhamDung05 <92784761+PhamDung05@users.noreply.github.com>
2023-05-11 17:23:06 +0700

    Add files via upload

---
+++ GeometricObject.java
@@ -0,0 +1,6 @@
+package BT7;
+
+public interface GeometricObject {
+    double getArea();
+    double getPerimeter();
+}
---
+++ Movable.java
@@ -0,0 +1,8 @@
+package BT6;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
